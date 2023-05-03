ab5a3d1dcc312b5c19badbd7a7c5b3826db413e4
Ninoverstraeten2003 <r0898606@ucll.be>
2023-05-03 17:47:16 +0200

    my first snapshot

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace View
         {
             InitializeComponent();
         }
+
+        private void ConvertToCelcius(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this, "You clicked the button", "Clicked!");
+            textBox.Text = "You clicked the button";
+        }
     }
 }
