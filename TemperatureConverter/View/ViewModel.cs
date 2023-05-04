373b4a8b26db91548f12ad41b7b976df82f07825
Ninoverstraeten2003 <r0898606@ucll.be>
2023-05-04 18:14:02 +0200

    refactoring done

--- TemperatureConverter/View/ViewModel.cs
+++ TemperatureConverter/View/ViewModel.cs
@@ -7,11 +7,15 @@ using System.Threading.Tasks;
 
 namespace View
 {
-    public class ConverterViewModel: INotifyPropertyChanged
+    public class ConverterViewModel : INotifyPropertyChanged
     {
-        public ConverterViewModel() { }
         private double temperatureInKelvin;
         public event PropertyChangedEventHandler PropertyChanged;
+        public ConverterViewModel() {
+            this.Kelvin = new TemperatureScaleViewModel(this, new KelvinTemperatureScale());
+            this.Celsius = new TemperatureScaleViewModel(this, new CelsiusTemperatureScale());
+            this.Fahrenheit = new TemperatureScaleViewModel(this, new FahrenheitTemperatureScale());
+        }
         public double TemperatureInKelvin
         {
             get { return temperatureInKelvin; }
@@ -21,5 +25,41 @@ namespace View
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
             }
         }
+        public TemperatureScaleViewModel Kelvin { get; }
+        public TemperatureScaleViewModel Celsius { get; }
+        public TemperatureScaleViewModel Fahrenheit { get; }
+
+        public IEnumerable<TemperatureScaleViewModel> Scales
+        {
+            get
+            {
+                yield return Celsius;
+                yield return Fahrenheit;
+                yield return Kelvin;
+            }
+        }
+    }
+    public class TemperatureScaleViewModel: INotifyPropertyChanged
+    {
+        private readonly ConverterViewModel parent;
+        private readonly ITemperatureScale temperatureScale;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name => temperatureScale.Name;
+
+        public double Temperature {
+            get { return temperatureScale.ConvertFromKelvin(parent.TemperatureInKelvin); }
+            set { parent.TemperatureInKelvin = temperatureScale.ConvertToKelvin(value); }
+        }
+
+        public TemperatureScaleViewModel(ConverterViewModel parent,ITemperatureScale temperatureScale)
+        {
+            this.parent = parent;
+            this.temperatureScale = temperatureScale;
+
+            this.parent.PropertyChanged += (sender, args) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Temperature)));
+        }
+
+
     }
 }
