bc6410838b54376bd80b42df360b0cc6ff742907
Ninoverstraeten2003 <r0898606@ucll.be>
2023-05-04 13:52:18 +0200

    moving scales in seperate folder

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -27,12 +27,6 @@ namespace View
         }
         
     }
-    public interface ITemperatureScale
-    {
-        string Name { get; }
-        double ConvertToKelvin(double temperature);
-        double ConvertFromKelvin(double temperature);
-    }
     public class TemperatureConverter : IValueConverter
     {
         public ITemperatureScale TemperatureScale { get; set; }
@@ -49,50 +43,6 @@ namespace View
             var temperature = double.Parse((string)value);
 
             return this.TemperatureScale.ConvertToKelvin(temperature);
-        }
-    }
-    public class KelvinTemperatureScale : ITemperatureScale
-    {
-        public string Name => "Kelvin";
-
-        public double ConvertFromKelvin(double temperature)
-        {
-            return temperature;
-        }
-
-        public double ConvertToKelvin(double temperature)
-        {
-            return temperature;
-        }
-    }
-
-    public class CelsiusTemperatureScale : ITemperatureScale
-    {
-        public string Name => "Celsius";
-
-        public double ConvertFromKelvin(double temperature)
-        {
-            return temperature - 273.15;
-        }
-
-        public double ConvertToKelvin(double temperature)
-        {
-            return temperature + 273.15;
-        }
-    }
-
-    public class FahrenheitTemperatureScale : ITemperatureScale
-    {
-        public string Name => "Fahrenheit";
-
-        public double ConvertFromKelvin(double temperature)
-        {
-            return temperature * 1.8 - 459.67; ;
-        }
-
-        public double ConvertToKelvin(double temperature)
-        {
-            return (temperature + 459.67) / 1.8;
         }
     }
 }
