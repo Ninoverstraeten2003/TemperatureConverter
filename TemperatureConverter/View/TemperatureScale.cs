8471b6136d32750ed6dc59d1b84eac5020355099
Ninoverstraeten2003 <r0898606@ucll.be>
2023-05-04 11:51:37 +0200

    working on refactoring

---
+++ TemperatureConverter/View/TemperatureScale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    internal class KelvinTemperatureScale:ITemperatureScale
+    {   
+        public string Name { get;  }
+        public double ConvertToKelvin(double temperature)
+        {
+            return 0.0;
+        }
+        public double ConvertFromKelvin(double temperature)
+        {
+            return temperature;
+        }
+    }
+}
