812dcf245bda977d1b923312098c8c3e90a66138
Ninoverstraeten2003 <r0898606@ucll.be>
2023-05-11 13:10:45 +0200

    done

--- TemperatureConverter/View/TemperatureScaleControl.xaml.cs
+++ TemperatureConverter/View/TemperatureScaleControl.xaml.cs
@@ -1,12 +1,29 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace View
 {
-    public partial class TemperatureScaleControl : Window
+    public partial class TemperatureScaleControl : UserControl
     {
         public TemperatureScaleControl()
         {
             InitializeComponent();
         }
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); } 
+            set { SetValue(HeaderProperty,value); } 
+        }
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string),
+            typeof(TemperatureScaleControl), new PropertyMetadata(""));
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); } set { SetValue(ValueProperty,value);} }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double),
+            typeof(TemperatureScaleControl),new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
     }
 }
